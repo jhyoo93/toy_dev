41b86e47dd8ef96fd4cd2c6bc00b2657fe4892f9
DESKTOP-CS6T6U3\ELUO <jh.yoo@eluocnc.com>
2024-06-24 13:36:17 +0900

    삭제

--- mall-project/components/ButtonLink.js
+++
@@ -1,6 +0,0 @@
-import Link from 'next/link';
-import styles from './Button.module.css';
-
-export default function ButtonLink({ className = '', ...props }) {
-  return <Link className={`${styles.button} ${className}`} {...props} />;
-}
